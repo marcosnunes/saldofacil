36fb86db62120868dac0d8be8711e97a5d78ebef
marcosnunes <marcos.nunes.lph@outlook.com>
2025-03-01 07:53:33 -0300

    Update

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyAPvYiaaGlHaJMaJzXoifWHBmp1be04F80",
-    authDomain: "web-app-autossuficiencia.firebaseapp.com",
-    databaseURL: "https://web-app-autossuficiencia-default-rtdb.firebaseio.com",
-    projectId: "web-app-autossuficiencia",
-    storageBucket: "web-app-autossuficiencia.appspot.com",
-    messagingSenderId: "702286825063",
-    appId: "1:702286825063:web:f16256bd9bfc7de3dcb6c2"
+  apiKey: "AIzaSyAPvYiaaGlHaJMaJzXoifWHBmp1be04F80",
+  authDomain: "web-app-autossuficiencia.firebaseapp.com",
+  databaseURL: "https://web-app-autossuficiencia-default-rtdb.firebaseio.com",
+  projectId: "web-app-autossuficiencia",
+  storageBucket: "web-app-autossuficiencia.appspot.com",
+  messagingSenderId: "702286825063",
+  appId: "1:702286825063:web:f16256bd9bfc7de3dcb6c2",
 };
 
 export default firebaseConfig;
--- utils.js
+++ utils.js
@@ -1,9 +1,9 @@
 export function displayYearInTitle() {
-    const urlParams = new URLSearchParams(window.location.search);
-    const currentYear = urlParams.get('year');
-    if (currentYear) {
-      const titleSpan = document.querySelector(".brand-logo.center"); // Corrigido o seletor
-      const currentTitle = titleSpan.textContent;
-      titleSpan.textContent = currentTitle + " " + currentYear;
-    }
+  const urlParams = new URLSearchParams(window.location.search);
+  const currentYear = urlParams.get("year");
+  if (currentYear) {
+    const titleSpan = document.querySelector(".brand-logo.center"); // Corrigido o seletor
+    const currentTitle = titleSpan.textContent;
+    titleSpan.textContent = currentTitle + " " + currentYear;
   }
+}
